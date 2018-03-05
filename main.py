4a0f3ca5ebf7d6cbda2823f2b12e8104f52e2ef3
Ksenia Sorokina <kseniasorokina@MacBook-Air-Ksenia.local>
2018-03-05 22:02:09 +0100

    config test 2

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import json
 import time
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-import sys
 import keboola
 from keboola import docker
 
 cfg = docker.Config('/data/')
-payload = cfg.get_parameters()
-print(payload)
+configuration = cfg.get_parameters()
+print(configuration)
 
+payload = {
+    'host': configuration['host'],
+    'apikey': configuration['#apikey'],
+    'user_id': configuration['user_id'],
+    'limit': configuration['limit'],
+    'date_range': configuration['date_range'],
+    'metrics': configuration['metrics'],
+    'subdomain': configuration['subdomain'],
+    'dimensions': configuration['dimensions']
+}
 
 def requests_retry_session(
     retries=5,   # repeats 5 times
@@ -36,14 +45,14 @@ first_api_call = requests.get('http://api.chartbeat.com/query/v2/submit/page/?',
 status_code = first_api_call.status_code
 if status_code != 200:
     print('Error, status code of first call: ' + str(status_code))
-    quit()
+    exit(2)
 
 # generate query_id as a result of first API call
 result_of_first_api_call = first_api_call.json()
 query_id = str(result_of_first_api_call['query_id'])
 
 # second API call, repeats every 5 min (300 vtr)
-pa1= {'host': payload['host'], 'apikey': payload['#apikey'], 'query_id': query_id}
+pa1= {'host': payload['host'], 'apikey': payload['apikey'], 'query_id': query_id}
 
 t0 = time.time()
 try:
@@ -53,7 +62,7 @@ try:
     )
 except Exception as x:
     print('It failed 😞', x.__class__.__name__)
-    sys.exit()
+    exit(2)
 else:
     print('It eventually worked', second_api_call.status_code)
 finally:
@@ -64,7 +73,7 @@ finally:
 status_code = second_api_call.status_code
 if status_code != 200:
 	print('Error, second response failed. Status code: ' + str(status_code))
-	sys.exit()
+	exit(2)
 
 # result of second API call as text object 
 result_of_second_api_call = second_api_call.text
