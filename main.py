544b4368cfc6331bd780323bf6b8f0a180920346
Ksenia Sorokina <kseniasorokina@MacBook-Air-Ksenia.local>
2018-04-06 16:25:58 +0200

    new parameters start and end; + par name

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 import keboola
 from keboola import docker
+import datetime
 
 cfg = docker.Config('/data/')
 configuration = cfg.get_parameters()
-outFullName = '/data/out/tables/ex_chartbeat.csv'
+name = configuration['table_name']
+outFullName = '/data/out/tables/' + name + '.csv'
 outDestination = 'ex_chartbeat'
+
+
+start = int(configuration['start'])
+today = datetime.datetime.now()
+DD_start = datetime.timedelta(days=start)
+earlier_start = today + DD_start
+earlier_str_start = earlier_start.strftime("%Y-%m-%d")
+
+
+end = int(configuration['end'])
+DD_end = datetime.timedelta(days=end)
+earlier_end = today + DD_end
+earlier_str_end = earlier_end.strftime("%Y-%m-%d")
 
 payload = {
     'host': configuration['host'],
     'apikey': configuration['#apikey'],
     'user_id': configuration['user_id'],
     'limit': configuration['limit'],
-    'date_range': configuration['date_range'],
+    'start': earlier_str_start,
+    'end': earlier_str_end,
     'metrics': configuration['metrics'],
     'subdomain': configuration['subdomain'],
     'dimensions': configuration['dimensions'],
@@ -25,6 +41,13 @@ payload = {
     'sort_order': configuration['sort_order'],
     'primary_key': configuration['primary_key']
 }
+
+'''
+from datetime import datetime, timedelta
+start = payload['start']
+
+date_N_days_ago = datetime.now() - timedelta(days=start)
+'''
 
 pk = payload['primary_key']
 cfg.write_table_manifest(outFullName, destination=outDestination, primary_key=pk, incremental=True)
@@ -47,12 +70,14 @@ def requests_retry_session(
     session.mount('http://', adapter)
     session.mount('https://', adapter)
     return session
-
+# read in/state.json and try to get query_id
+#if query id is empty then continue
+# else skip to second api call
 # first API call, repeates every 180 sec
 t0 = time.time()
 try:
     first_api_call = requests_retry_session().get(
-    'http://api.chartbeat.com/query/v2/submit/page/?', params=payload,  
+    'http://api.chartbeat.com/query/v2/submit/page/?', params=payload,
     timeout=180
     )
     print(first_api_call )
@@ -77,6 +102,7 @@ result_of_first_api_call = first_api_call.json()
 query_id = str(result_of_first_api_call['query_id'])
 print("query_id: ",  query_id)
 
+# else part starts here
 # second API call, repeats every 180 sec
 pa1= {'host': payload['host'], 'apikey': payload['apikey'], 'query_id': query_id}
 
@@ -87,6 +113,7 @@ try:
     timeout=180
     )
     print(second_api_call)
+    
 except Exception as x:
     print('It failed 😞 (second_api_call)', x.__class__.__name__)
 else:
@@ -108,6 +135,7 @@ result_of_second_api_call = second_api_call.text
 
 
 # result of second API call as CSV 
+#save query_id to data/out/state.json {'query_id': query_id}
 file = open(outFullName,'w')  
 file.write(result_of_second_api_call)
 file.close()
